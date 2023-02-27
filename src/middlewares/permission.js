29de9fd56dafa45ca2c27b5056b3bac6de691df4
ShivamRathore07 <shivamrathoreji07@gmail.com>
2023-02-27 12:53:09 +0530

    update individual field

--- src/middlewares/permission.js
+++ src/middlewares/permission.js
@@ -3,7 +3,6 @@ import jwt from "jsonwebtoken";
 const permission = (...allowedRoles) => {
   return (req, res, next) => {
     const token = req.cookies["_user_"];
-    console.log("06",token)
 
     if (!token) return res.sendStatus(401);
 
@@ -11,16 +10,12 @@ const permission = (...allowedRoles) => {
       let decodedData;
 
       decodedData = jwt.verify(token, process.env.SECRET_KEY);
-      console.log("14",decodedData)
 
       const rolesArray = [decodedData.user.role];
-      console.log("17",rolesArray)
-
       const result = rolesArray
         .map((role) => allowedRoles.includes(role))
         .find((val) => val === true);
-        console.log("22",result)
-
+  
       if (!result) return res.sendStatus(401);
 
       next();
