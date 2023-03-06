60d66b27996df85157f17308291854b828581b72
ShivamRathore07 <shivamrathoreji07@gmail.com>
2023-03-06 18:12:54 +0530

    complete freature

--- src/models/price/priceModel.js
+++ src/models/price/priceModel.js
@@ -12,6 +12,13 @@ const priceSchema = mongoose.Schema(
     standardFeature:{ type:[String], required: true},
     proFeature:{ type:[String], required: true},
     enterpriseFeature:{ type:[String], required: true},
+    plan:{ type:[Object], required: true},
+    team:{ type:[Object], required: true},
+    wall:{ type:[Object], required: true},
+    file:{ type:[Object], required: true},
+    product:{ type:[Object], required: true},
+    manage:{ type:[Object], required: true},
+    dashboard:{ type:[Object], required: true},
   },
 );
 
